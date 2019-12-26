989af3ea4a5be57cfa3b00c6637ca33dd6f83937
shenweiquan <17806533235@163.com>
2019-12-26 10:57:42 +0800

    idae 结合github 新增test

--- src/com/swq/NiuKe/test.java
+++ src/com/swq/NiuKe/test.java
@@ -2,6 +2,6 @@ package com.swq.NiuKe;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World1111");
     }
 }
